9d0e3498d6640e41be716e360bd530b7df07d29c
DavidArthurCole <40234707+DavidArthurCole@users.noreply.github.com>
2023-01-26 22:12:09 -0500

    Update for 1.19.3

--- src/main/java/com/davidarthurcole/bhb/mixin/AWTHeadlessMixin.java
+++ src/main/java/com/davidarthurcole/bhb/mixin/AWTHeadlessMixin.java
@@ -1,5 +1,6 @@
 package com.davidarthurcole.bhb.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +12,7 @@ import java.util.Locale;
 @Mixin(Main.class)
 public class AWTHeadlessMixin
 {
-    @Inject(at = @At("HEAD"), method = "main")
+    @Inject(at = @At("HEAD"), method = "main([Ljava/lang/String;)V")
     private static void init(CallbackInfo info) {
         if (!System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("mac")) {
             System.out.println("[BHB - Blazin's Hex Blender] Setting java.awt.headless to false");
